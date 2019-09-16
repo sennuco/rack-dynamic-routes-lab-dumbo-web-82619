716d5b131be60bbb747129b7eff8ea52a54fbeb6
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-16 12:18:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last 
-      item = 
+      item = @@items.find{|i| item.name == item_name}
     
   
   
