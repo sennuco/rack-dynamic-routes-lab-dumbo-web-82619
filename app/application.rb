70fa39a34ae9b31107d65a7339d0d624fc704294
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-16 12:12:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,23 +5,7 @@ class Application
     resp = Rack::Response.new 
     req= Rack::Request.new
     
-    if req.path.match(/items/)
-      item_name = req.path.split("/items/").last 
-      item = @@item.find{|i| i.name == item_name}
-      
-      if item 
-        resp.write "#{item.price}"
-      else 
-        resp.write "Item not found"
-        resp.status = 400
-      end
   
-  else
-      resp.write "Route not found"
-      resp.status = 404
-   end
-        
-  end
   
   
 end 
