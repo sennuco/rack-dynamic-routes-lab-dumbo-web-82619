258c63e72e4921e503fd07bd232918aa24198984
Theresa Cyrus <cyrustheresa@gmail.com>
2019-09-16 12:17:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
-      item_name = req.path.split("/items/")
+      item_name = req.path.split("/items/").last 
     
   
   
